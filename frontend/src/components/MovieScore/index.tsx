8e45de6cc294e1508661f5a2ad1b7097505f96ed
Caio Reis <caionohamachi@gmail.com>
2022-01-17 01:17:41 -0300

    First request

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -8,7 +8,7 @@ function MovieScore() {
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+                <MovieStars />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
